515bc2dd1b8bf59d6884f1ed53ed86308020aaab
unknown <lopez.thinktank@gmail.com>
2022-08-03 14:40:34 -0700

    Added some more varibles and finished the special characters array- also implemented the confirm method

--- Developer/script.js
+++ Developer/script.js
@@ -1,14 +1,77 @@
+
+
+// Added additional variables to complete the program and makeup a password with random numbers and letters.
+var bigAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"];
+var lilAlpha = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n","o", "p", "q", "r", "s", "t", "u","v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.',];
+
+// var calculations -- this is used to combine all the arrays for an answer. 
+var calculations = [];
+var userChoice = [];
+
+// This variable will ask to include special characters
+var inclSpecialCharacters = confirm("Click OK to include special characters.");
+
+// This will ask to include numbered characters
+var inclNumbers = confirm("Click OK to inlcude numbered characters.");
+
+// This variable asks about lowercase characters
+var inclLittleLetters = confirm("Click OK to include lowercase characters.");
+
+// This variable asks about uppercase characters
+var inclBigLetters = confirm("Click OK to include uppercase characters.");
+
+
+
+
+// This function is required to make the existing code work and will control the process of generating a password.
+function generatePassword(i) {
+  for (var i = 0; i <= length; i++);
+  // var userChoice = ([]);
+  prompt("Please enter a number between 8 and 33 characters in length.");
+
+  var bigLetters = bigAlpha[Math.floor(Math.random()*bigAlpha.length)];
+    console.log(bigLetters);
+
+  var littleLetters = lilAlpha[Math.floor(Math.random()*lilAlpha.length)];
+    console.log(littleLetters);
+
+  var digits = numbers[Math.floor(Math.random()*numbers.length)];
+    console.log(digits);
+  
+  var special = specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+    console.log(special); 
+
+  if (length < 8) {
+    alert("Password must be between 8 and 33 characters long. Try again Please.");
+    return null;
+  }
+
+  if (length > 33) {
+    alert("Password must be less than 33 characters long. Please try again.");
+    return null;
+  }
+
+};
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+ 
 }
 
-// Add event listener to generate button
+// Adds event listener to the generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
